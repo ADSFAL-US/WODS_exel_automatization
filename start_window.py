8b7751aab8c8f351b68c8822ea298c0403a714d0
adsfal-us <adsfal.gg@gmail.com>
2025-04-24 20:18:25 +0300

    update gui

--- start_window.py
+++ start_window.py
@@ -20,14 +20,16 @@ class CreateDatabaseDialog(tk.Toplevel):
         self.grab_set()
     
     def _setup_ui(self):
-        tk.Label(self, text="""Название базы данных без ".db":""").pack(pady=5)
-        entry = tk.Entry(self, textvariable=self.db_name)
+        tk.Label(self, text="""Название базы данных без ".db":""", bg = "#383838", fg = "#ffffff").pack(pady=5)
+        entry = tk.Entry(self, textvariable=self.db_name, bg = "#383838", fg = "#ffffff")
         entry.pack(pady=5)
         entry.bind("<Return>", lambda e: self._create_db())
         
-        btn_frame = tk.Frame(self)
+        btn_frame = tk.Frame(self, background="#383838")
         btn_frame.pack(pady=5)
         
+        self.configure(bg="#383838")
+        self.parent.configure(bg="#383838")
         tk.Button(btn_frame, text="Создать", command=self._create_db, fg="#ffffff", bg="#11f018").pack(side=tk.LEFT, padx=5)
         tk.Button(btn_frame, text="Отмена", command=self.destroy, fg="#ffffff", bg="#f01111").pack(side=tk.RIGHT, padx=5)
     
@@ -67,25 +69,47 @@ class StartWindow(tk.Tk):
         self.refresh_table_list()
         self.current_app_window = None
         self.app_window = None
+        self._init_style()
+    
+    def _init_style(self):
+        style = ttk.Style()
+        style.theme_use('default')
+        
+        style.configure(
+            "Custom.Treeview",
+            background="#383838",  # Цвет фона
+            fieldbackground="#383838",  # Цфон области данных
+            foreground="white"  # Цвет текста
+        )
+
+        # Настраиваем цвета для выделенных элементов
+        style.map(
+            "Custom.Treeview",
+            background=[('selected', '#606060')],
+            foreground=[('selected', 'white')]
+        )
     
     def _setup_ui(self):
         # Фрейм для списка таблиц
-        list_frame = tk.Frame(self, width=200)
+        list_frame = tk.Frame(self, width=200, background="#383838")
         list_frame.pack(side=tk.LEFT, fill=tk.Y, padx=5, pady=5)
         
-        self.table_list = ttk.Treeview(list_frame, show="tree", selectmode="browse")
+        self.table_list = ttk.Treeview(list_frame, show="tree", selectmode="browse", style="Custom.Treeview")
         self.table_list.pack(fill=tk.BOTH, expand=True)
         self.table_list.bind("<Double-1>", self._on_table_selected)
         
         # Центральная кнопка
-        center_frame = tk.Frame(self)
+        self.configure(bg = "#383838")
+        center_frame = tk.Frame(self, bg = "#383838")
         center_frame.pack(expand=True, fill=tk.BOTH)
         
         self.create_btn = tk.Button(
             center_frame,
             text="+ Создать таблицу",
             command=self.show_create_dialog,
-            font=("Arial", 14, "bold")
+            font=("Arial", 14, "bold"),
+            bg="#11f018",
+            fg = "#ffffff"
         )
         self.create_btn.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
     
